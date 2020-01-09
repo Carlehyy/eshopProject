5feae9bb95a8a8b071f85ce86ac4efa31f6dd839
huyiyang <1040787244@qq.com>
2020-01-09 20:03:12 +0800

    miao

--- src/cn/web/servlet/IndexServlet.java
+++ src/cn/web/servlet/IndexServlet.java
@@ -1,6 +1,9 @@
 package cn.web.servlet;
 
-import lombok.Data;
+
+import cn.web.entity.*;
+
+import cn.web.service.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,13 +12,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * 主页面信息
- */
+import java.sql.SQLException;
+import java.util.List;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
+    private ProductTypeService productTypeService = new ProductTypeService();
+
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+//        try {
+////            User user = (User) req.getSession().getAttribute("user");
+////            System.out.println(user);
+//            //查询所有分类
+//            List<ProductType> list = productTypeService.productTypes();
+//            resp.setHeader("content-type","img/jpeg");
+//            req.setAttribute("list", list);
+//
+//            //新品
+//            List<ListGoods> list1 = ListGoodsService.listGoods();
+//            req.setAttribute("list1", list1);
+//            //排行榜
+//
+//            //全球进口
+//
+//            //服装服饰
+//            List<Clothing> list2 = ClothingService.clothing();
+//            req.setAttribute("list2", list2);
+//
+//            //护肤美妆
+//            List<SkinCare> list3 = SkinCareService.skincare();
+//            req.setAttribute("list3",list3);
+//
+//            //图书学习
+//            List<LibraryS> list4= LibrarysService.librarysService();
+//            req.setAttribute("list4",list4);
+//
+//            req.getRequestDispatcher("index.jsp").forward(req, resp);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
+
